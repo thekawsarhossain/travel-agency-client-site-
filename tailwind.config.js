f6aa8eccdb445ffa6e6ab57cf229fe590c9331da
thekawsarhossain <thekawsarhossain@gmail.com>
2022-01-26 16:45:00 +0600

    navigation initial setup done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,15 @@
 module.exports = {
+  // mode: "jit",
+  // purge: ["./src/Components/**/*.{html,js}"],
+  // we can use here "class" if we want to control this datk mode manually
+  darkMode: "media",
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      container: {
+        center: true,
+      },
+    },
   },
   plugins: [],
 };
