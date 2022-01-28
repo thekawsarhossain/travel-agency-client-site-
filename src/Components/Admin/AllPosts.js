fd723ca44942829b4a95c1aeeb5b895cbd2ed71e
thekawsarhossain <thekawsarhossain@gmail.com>
2022-01-28 21:27:40 +0600

    post approve and delete method added

--- src/Components/Admin/AllPosts.js
+++ src/Components/Admin/AllPosts.js
@@ -3,6 +3,7 @@ import { useSelector } from "react-redux";
 import Rating from "react-rating";
 import { useDispatch } from "react-redux";
 import { userPosts } from "../../Redux/Slices/blogSlice";
+import { toast, ToastContainer } from "react-toastify";
 
 const AllPosts = () => {
   const dispatch = useDispatch();
@@ -10,58 +11,102 @@ const AllPosts = () => {
     dispatch(userPosts());
   }, [dispatch]);
 
-  const blogs = useSelector((state) => state.blog?.userPosts);
+  const blogs = useSelector((state) => state?.blogs?.userPosts);
+
+  // approve handler button here
+  const handleApprove = (blog) => {
+    fetch("http://localhost:5000/blogs", {
+      method: "POST",
+      headers: { "content-type": "application/json" },
+      body: JSON.stringify(blog),
+    })
+      .then((res) => res.json())
+      .then((result) => {
+        if (result.insertedId) {
+          toast("post approved");
+          fetch(`http://localhost:5000/blog/${blog?._id}`, {
+            method: "DELETE",
+            headers: { "content-type": "application/json" },
+          })
+            .then((res) => res.json())
+            .then((result) => console.log(result));
+        }
+      });
+  };
+
+  // handle delete here
+  const handleDelete = (id) => {
+    fetch(`http://localhost:5000/blog/${id}`, {
+      method: "DELETE",
+      headers: { "content-type": "application/json" },
+    })
+      .then((res) => res.json())
+      .then((result) => console.log(result));
+  };
 
   return (
-    <div className="w-11/12">
+    <div className="w-11/12 mx-auto">
       <div className="grid grid-cols-1 sm:grid-cols-1 gap-8 py-8">
         {/* map items here  */}
-        {blogs?.blogs?.map((blog) => (
-          <div
-            key={blog?._id}
-            className="border rounded-md border-gray-800 block lg:flex justify-start items-center relative"
-          >
-            <img src={blog?.locationImage} className="lg:w-5/12" alt="" />
-            <div className="p-2">
-              <div>
-                <img
-                  src={blog.userImage}
-                  alt="user"
-                  className="w-12 sm:absolute top-0 right-0 rounded-full hidden lg:block"
-                />
-                <h6>
-                  {" "}
-                  <span className="font-medium">Added By: </span>{" "}
-                  {blog?.userName}
-                </h6>
-                <p>
-                  <span className="font-medium">Location:</span>{" "}
-                  {blog?.location}
-                </p>
-                <p>
-                  <span className="font-medium">Opinion: </span>
-                  {blog?.userThoughts?.length > 40
-                    ? blog?.userThoughts?.split(" ").slice(0, 18).join(" ")
-                    : "You need more text to add "}
-                  ...
-                </p>
-                <div className="flex justify-between items-start">
-                  <Rating
-                    className="text-golden"
-                    initialRating={blog?.rating}
-                    emptySymbol="far fa-star"
-                    fullSymbol="fas fa-star"
-                    readonly
-                  />
-                  <button className="btn-small mt-2" onCLick>
-                    Approve
-                  </button>
-                  <button className="btn-small mt-2">Delete</button>
+        {!blogs.length ? (
+          <h2 className="text-center text-primary">
+            There is no post to apporove !
+          </h2>
+        ) : (
+          blogs?.map((blog) => (
+            <div
+              key={blog?._id}
+              className="border rounded-md border-gray-800 block lg:flex justify-start items-center "
+            >
+              <img src={blog?.locationImage} className="lg:w-5/12" alt="" />
+              <div className="p-2">
+                <div>
+                  <h6>
+                    {" "}
+                    <span className="font-medium">Added By: </span>{" "}
+                    {blog?.userName}
+                  </h6>
+                  <p>
+                    <span className="font-medium">Location:</span>{" "}
+                    {blog?.location}
+                  </p>
+                  <p>
+                    <span className="font-medium">Opinion: </span>
+                    {blog?.userThoughts?.length > 40
+                      ? blog?.userThoughts?.split(" ").slice(0, 18).join(" ")
+                      : "You need more text to add "}
+                    ...
+                  </p>
+                  <div className="flex  items-center">
+                    <h6 className="font-medium">Given Rate: </h6>
+                    <Rating
+                      className="text-golden"
+                      initialRating={blog?.rating}
+                      emptySymbol="far fa-star"
+                      fullSymbol="fas fa-star"
+                      readonly
+                    />{" "}
+                  </div>
+                  <div>
+                    <button
+                      className="btn-small mt-2 "
+                      onClick={() => handleApprove(blog)}
+                    >
+                      Approve
+                    </button>
+                    <button
+                      className="btn-small mt-2 mx-2"
+                      onClick={() => handleDelete(blog?._id)}
+                    >
+                      Delete
+                    </button>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-        ))}
+          ))
+        )}
+        <ToastContainer />
       </div>
     </div>
   );
